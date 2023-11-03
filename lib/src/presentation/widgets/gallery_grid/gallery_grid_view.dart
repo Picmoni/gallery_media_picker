1965e3c8d0304bfe6b058ab2448790db2ec40c5d
BrianTum <brianktum@gmail.com>
2023-11-03 17:04:47 +0300

    reverting changes

--- lib/src/presentation/widgets/gallery_grid/gallery_grid_view.dart
+++ lib/src/presentation/widgets/gallery_grid/gallery_grid_view.dart
@@ -162,16 +162,4 @@ class GalleryGridViewState extends State<GalleryGridView> {
       }
     }
   }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) {
-      return true;
-    }
-    if (other.runtimeType != runtimeType) {
-      return false;
-    }
-
-    return other != this;
-  }
 }
